b2c6a8e060d1b82a2471701f1dfae28ebba775e4
emmrey <okparaemmanuelanyanso@gmail.com>
2023-09-11 13:28:52 +0100

    second commit

--- StartUp.py
+++ StartUp.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import pandas as pd
-import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
 import sklearn
@@ -33,9 +32,9 @@ st.markdown("<h2 style = 'top-margin: 0rem;text-align: center; color: #A2C579'>P
 
 st.write("In the dynamic landscape of entrepreneurship, understanding and predicting the profitability of startup firms is crucial for investors, founders, and stakeholders alike. The project at hand employs a Linear Regression model to analyze and forecast the profitability of startup companies. Leveraging historical data on various startups, this study aims to unravel the key factors that influence a startup's success and provide valuable insights for making informed investment decisions. By delving into the intricate relationship between variables such as R&D expenditure, marketing spend, location, and industry, this project endeavors to shed light on the critical drivers of startup profitability, contributing to a more data-driven approach in the world of business.")
 
-heat_map = plt.figure(figsize = (14,7))
-correlation_data = data[['R&D Spend',	'Administration',	'Marketing Spend', 'Profit']]
-sns.heatmap(correlation_data.corr(), annot = True, cmap = 'BuPu')
+# heat_map = plt.figure(figsize = (14,7))
+# correlation_data = data[['R&D Spend',	'Administration',	'Marketing Spend', 'Profit']]
+# sns.heatmap(correlation_data.corr(), annot = True, cmap = 'BuPu')
 
 st.write(heat_map)
 data.drop('Unnamed: 0', axis = 1, inplace = True)
